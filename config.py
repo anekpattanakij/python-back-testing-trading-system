8bd52ad51ae10b00795b0cd02ce718ab30bff30f
DESKTOP-J7JJU4M\a.anekpattanakij <a.anekpattanakij@gmail.com>
2022-04-22 17:43:40 +0700

    adjust market restriction to be in decimal type for precise calculation

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+from decimal import Decimal
 market_price_range = {"UP": 1.3, "DOWN": 0.7}
-min_order_amount = {"BTCUSDT": 0.000001, "ETHUSDT": 0.00001, "BNBUSDT": 0.0001}
-min_order_usdt = {"BTCUSDT": 10, "ETHUSDT": 10 , "BNBUSDT": 10}
-order_spread_usdt = {"BTCUSDT": 0.01, "ETHUSDT": 0.01, "BNBUSDT": 0.01}
-
+min_order_amount = {"BTCUSDT": Decimal(0.000001), "ETHUSDT": Decimal(
+    0.00001), "BNBUSDT": Decimal(0.0001)}
+min_order_usdt = {"BTCUSDT": Decimal(
+    10), "ETHUSDT": Decimal(10), "BNBUSDT": Decimal(10)}
+order_spread_usdt = {"BTCUSDT": Decimal(
+    0.01), "ETHUSDT": Decimal(0.01), "BNBUSDT": Decimal(0.01)}
